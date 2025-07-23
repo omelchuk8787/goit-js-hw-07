e953c84b85c505f244562448795b8a561f46f312
unknown <you@example.comomelchuk.8787@gmail.com>
2025-07-23 21:34:15 +0300

    all

--- js/task-1.js
+++ js/task-1.js
@@ -4,11 +4,11 @@ const categoryItems = categoriesList.querySelectorAll('.item');
 
 console.log(`Number of categories: ${categoryItems.length}`);
 
-categoryItems.forEach(item => { 
-  const categoryName = item.querySelector('h2').textContent;
-
-  const elementsCount = item.querySelectorAll('ul li').lenght;
-
-  console.log(`Category: ${categoryName}`);
-  console.log(`Elements: ${elementsCount}`);
-});
+categoryItems.forEach(item => {
+    const categoryName = item.querySelector('h2').textContent;
+  
+    const elementsCount = item.querySelectorAll('ul li').length;
+  
+    console.log(`Category: ${categoryName}`);
+    console.log(`Elements: ${elementsCount}`);
+  });
