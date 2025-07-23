9371ccebb5e9f33f845fe1e35857db444b41655d
unknown <you@example.comomelchuk.8787@gmail.com>
2025-07-23 21:11:15 +0300

    all

--- js/task-1.js
+++ js/task-1.js
@@ -1,42 +1,14 @@
-const getUserNames = users => users.map(user => user.name);
+const categiriesList = document.querySelector('#categories');
 
-console.log(
-  getUserNames([
-    {
-      name: "Moore Hensley",
-      email: "moorehensley@indexia.com",
-      balance: 2811
-    },
-    {
-      name: "Sharlene Bush",
-      email: "sharlenebush@tubesys.com",
-      balance: 3821
-    },
-    {
-      name: "Ross Vazquez",
-      email: "rossvazquez@xinware.com",
-      balance: 3793
-    },
-    {
-      name: "Elma Head",
-      email: "elmahead@omatom.com",
-      balance: 2278
-    },
-    {
-      name: "Carey Barr",
-      email: "careybarr@nurali.com",
-      balance: 3951
-    },
-    {
-      name: "Blackburn Dotson",
-      email: "blackburndotson@furnigeer.com",
-      balance: 1498
-    },
-    {
-      name: "Sheree Anthony",
-      email: "shereeanthony@kog.com",
-      balance: 2764
-    },
-  ])
-);
-// ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+const categoryItems = categoriesList.querySelectorAll('.item');
+
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+categoryItems.forEach(item => { 
+  const categoryName = item.querySelector('h2').textContent;
+
+  const elementsCount = item.querySelectorAll('ul li').lenght;
+
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${elementsCount}`);
+});
