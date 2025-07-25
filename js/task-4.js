ab907a9d625078ce49ef64b4f6b5add7df865e1e
unknown <you@example.comomelchuk.8787@gmail.com>
2025-07-25 20:05:10 +0300

    all

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,29 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault(); 
+
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (emailValue === '' || passwordValue === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData); 
+
+  form.reset(); 
+}
 
